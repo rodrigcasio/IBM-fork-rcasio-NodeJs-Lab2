d91db3a88c95543ce924494b13a8596dbc22f5eb
rodrigcasio <rod.casio@outlook.com>
2025-10-27 14:16:30 -0600

    Added IBM functionality for `PUT request` API within the `/user` route, also placed my own approach bellow the full code (commented)

--- routes/users.js
+++ routes/users.js
@@ -55,13 +55,46 @@ router.post('/', (req, res) => {      // based on the query parameters from the 
   res.status(200).json({ message: `The user ${req.query.firstName} has been added successfully`}); 
 });
 
+router.put('/:email', (req, res) => {
+  const email = req.params.email;
+  const filteredUsers = users.filter((user) => user.email === email);    // new array with only the user with the matched email
 
-// PUT request: Update the details of a user by email ID
-router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  if (flteredUser > 0) {
+    let filtered_user = filteredUsers[0];     // selecting the first matching user and upadate attributes if provided
+
+    // update DOB  or any other info:
+    let newDOB = req.query.DOB;
+    if (newDOB) {
+      filtered_user.DOB = newDOB;
+    }
+
+    let newFirstName = req.query.firstName;
+    if (newFirstName) {
+      filtered_user.firstName = newFirstName;
+    }
+    
+    let newLastName = req.query.lastName;
+    if (newLastName) {
+      filtered_user.lastName = newLastName;
+    }
+
+    let newEmail = req.query.email;
+    if (newEmail) {
+      filtered_user.email = newEmail;
+    }
+
+    // replacing the old user with the updated user  
+    // (modifying the 'users' db with a new array with all the users that dont match the :email, 
+    // like deleting the user with the matching ':email' and then adding it again with updated info in: (users.push(filtered_user) )
+    
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    
+    res.status(200).json({ message: `User with the email '${email}' successfully updated.` });
+  } else {
+      res.status(400).json({ message: `Unable to find user  with email '${email}'. Please try again.` });
+  }
 });
-
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
@@ -76,7 +109,7 @@ module.exports = router;
 /*
   (FIRST APROACHES FOR PRACTICE)
 
-
+1. 
 // POST request: Create a new user (first approach [not verified to be correct])
 router.post('/', (req, res) => {
   const newUser = req.body;
@@ -90,5 +123,42 @@ router.post('/', (req, res) => {
     message: `New user named: '${newUser.firstName}' added successfully`,
     email: newUser.email,
   } );
+});
+  ----------
+
+  2. update anything from an existing user: curl --request PUT 'localhost:5000/user/johnsmith@gamil.com?DOB=9/7/1999&firstName=Johnnathan'
+    (rcasio approach)
+    (WORKS well)
+
+router.put('/:email', (req, res) => {     // updating users DOB
+  const email = req.params.email;
+  const userToUpdate = users.find(user => user.email === email);
+
+  if (!userToUpdate) {
+    return res.status(400).json({ message: 'Invalid email, not available within our database' });
+  }
+
+  let newfirstName = req.query.firstName;
+  let newLastName = req.query.lastName;
+  let newEmail = req.query.email;
+  let newDOB = req.query.DOB;
+
+  if (newfirstName || newLastName || newEmail || newDOB) {
+    userToUpdate.firstName = newfirstName;
+    userToUpdate.lastName = newLastName;
+    userToUpdate.email = newEmail;
+    userToUpdate.DOB = newDOB;
+
+    return res.status(200).json({
+      message: `Updated info of user named: '${userToUpdate.firstName}'`,
+      lastname: `${userToUpdate.lastName}`,
+      email: `${userToUpdate.email}`,
+      DOB: `${userToUpdate.DOB}`
+    });
+
+  } else {
+
+    res.status(400).json({ message: `New info not provided within the request`});
+  }
 });
 */
